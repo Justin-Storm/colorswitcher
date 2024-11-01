3168edc4c41879da19c46c98579f0c189080c25b
Justin-Storm <144840724+Justin-Storm@users.noreply.github.com>
2024-11-01 13:30:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-const body = document.getElementById('body');
-const text = document.getElementById('text');
+const body = document.querySelector('body');
+const rgbtext = document.getElementById('output');
+
 
 function red() {
-    body.style.background = "red";
-    text.innerHTML = 'rgb(255, 0, 0)';
+  body.style.backgroundColor = "red";
+  rgbtext.innerHTML = 'rgb(255, 0, 0)';
 }
-
 function green() {
-    body.style.background = "limegreen";
-    text.innerHTML = 'rgb(0, 255, 0)';
+  body.style.backgroundColor = "limegreen";
+  rgbtext.innerHTML = 'rgb(0, 255, 0)';
 }
-
 function blue() {
-    body.style.background = "blue";
-    text.innerHTML = 'rgb(0, 0, 255)';
+  body.style.backgroundColor = "blue";
+  rgbtext.innerHTML = 'rgb(0, 0, 255)';
 }
 
 function random() {
-    const red = Math.round(Math.random() * 255);
-    const green = Math.round(Math.random() * 255);
-    const blue = Math.round(Math.random() * 255);
+  const red = Math.round(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
 
-    const randomColor = `rgb(${red}, ${green}, ${blue})`;
+  const rgbcode = `rgb(${red}, ${green}, ${blue})`;
 
-    body.style.background = randomColor;
-    text.innerHTML = randomColor;
+  body.style.backgroundColor = rgbcode;
+  rgbtext.innerHTML = rgbcode;
 }
+
+document.getElementById('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+  rgbtext.innerHTML = `rgb(${document.getElementById('red-value').value}, ${document.getElementById('green-value').value}, ${document.getElementById('blue-value').value})`;
+
+  body.style.backgroundColor = `rgb(${document.getElementById('red-value').value}, ${document.getElementById('green-value').value}, ${document.getElementById('blue-value').value})`;
+
+});
